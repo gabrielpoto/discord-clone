8d20ad99c51e83e5648b7960e45c238a86045c30
gabrielpoto <potogabriel1@gmail.com>
2024-03-03 17:42:47 -0500

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+=
+tsconfig.json,6/1/61ebb9fd6e8cf9082658121d5d81e297791dacd0
+B
+tailwind.config.ts,5/2/52c961376029165b06e7861b1dc68c3b2455eeff
+?
+components.json,c/7/c77795c1a93b077ad2aef052b6a976d9dc92f5b8
+?
+app/globals.css,4/d/4dfa49b1332116153eb16507891285845e9ab86e
+<
+app/page.tsx,6/f/6f76c4f4dad83442e836edc16c9735cb7bb32a73
---
+++ app/(main)/(route)/server/[serverId]/page.tsx
@@ -0,0 +1,20 @@
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  }
+};
+
+const ServerIdPage = async ({
+  params
+}: ServerIdPageProps) => {
+ 
+  return <div>test</div>
+}
+ 
+export default ServerIdPage;
